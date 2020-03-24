eba67d2c75bf8fcc982a8ef260ce7257f3ebf795
Hoplin <45956041+J-hoplin1@users.noreply.github.com>
2020-03-24 17:21:39 +0900

    Update LeagueofLegendSummonerSearchBot.py

--- Functions/LeagueofLegendSummonerSearchBot.py
+++ Functions/LeagueofLegendSummonerSearchBot.py
@@ -22,6 +22,9 @@ import warnings
 import youtube_dl
 warnings.filterwarnings(action='ignore')
 bot = commands.Bot(command_prefix='$')
+
+opggsummonersearch = 'https://www.op.gg/summoner/userName='
+
 '''
 asyncio : Asynchronous I/O. It is a module for asynchronous programming and allows CPU operations to be handled in parallel with I/O.
 
